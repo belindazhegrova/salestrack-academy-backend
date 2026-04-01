38953a2477d2a4665f1fe6c159dd34e469a47e22
Belinda Zhegrova <belindazhegrova1@gmail.com>
2026-04-01 16:02:24 +0200

    added direct url on prisma

---
+++ prisma.config.js
@@ -0,0 +1,10 @@
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: process.env["DATABASE_URL"],
+    directUrl: process.env["DIRECT_URL"],
+  },
+});
